9b43a64d30a6a4e1104f2211a83a4e4a9fbd1af2
kc <kajalchakole@yahoo.co.in>
2020-12-30 20:20:09 +0530

    Added React Router with Switch

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/, 
+            exclude: /node-modules/ 
+        },
+        {
+            test: /\.s?css$/,
+            exclude: /node-modules/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]        
+        }]
+    },
+    devtool: 'cheap-eval-module-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true
+    }
+};
