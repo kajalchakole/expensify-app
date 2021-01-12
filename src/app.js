a1ee24bd8f5de957cae223100f70ada3dc21e4b5
kc <kajalchakole@yahoo.co.in>
2021-01-12 21:29:21 +0530

    Providers and Connect in react expensify app

--- src/app.js
+++ src/app.js
@@ -1,33 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
-import './styles/style.scss';
+import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { addExpense } from "./actions/expenses";
 import { setTextFilter } from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
+import 'normalize.css/normalize.css';
+import './styles/style.scss';
 
 const store = configureStore();
 
-const expenseOne = store.dispatch(addExpense({
+store.dispatch(addExpense({
     description:'Water Bill',
-    amount: 100,
+    amount: 4500,
     createdAt: 1000
 }));
-
 store.dispatch(addExpense({
     description:'Gas Bill',
-    amount: 500,
+    amount: 109500,
     createdAt: 5000
 }));
 
-
-store.dispatch(setTextFilter('wat'));
-
+store.dispatch(addExpense({
+    description:'Rent',
+    amount: 1000,
+    createdAt: 2000
+}));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>    
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
---
+++ src/components/ExpenseListItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { connect } from "react-redux";
+import { removeExpense } from "../actions/expenses";
+
+const ExpenseListItem = ({description, amount, createdAt, id, dispatch}) => (
+    <div>
+        <h3>{description}</h3>
+        <p>{amount} - {createdAt}</p>
+        <button onClick={(e) => {
+            dispatch(removeExpense({id}))
+        }}>Remove</button>
+    </div>
+);
+
+const mapStateToProps = (state) => {
+    return {
+        expense : state.expense
+    };
+};
+
+export default connect(mapStateToProps)(ExpenseListItem);
