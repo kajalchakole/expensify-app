70bb11995eb82eeaa0ca2883604c953088788e54
kc <kajalchakole@yahoo.co.in>
2021-01-14 16:52:49 +0530

    Added form to add expense

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import moment from 'moment';
+// import { SingleDatePicker } from 'react-dates';
+// import 'react-dates/lib/css/_datepicker.css';
+
+const now = moment();
+console.log(now.format('YYYY/MM/DD'));
+
+export default class ExpenseForm extends React.Component {
+    state = {
+        description: '',
+        note: '',
+        amount: ''
+    };
+
+    onDescriptionChange = (e) => {
+        const description = e.target.value;
+        this.setState(() => ({ description }));
+    };
+
+    onNoteChange = (e) => {
+        const note = e.target.value;
+        this.setState(() => ({ note }));
+    };
+
+    onAmountChange = (e) => {
+        const amount = e.target.value;
+        if(amount.match(/^\d*(\.\d{0,2})?$/)){
+            this.setState(() => ({ amount }));
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input 
+                        type="text"
+                        placeholder="Description"
+                        autoFocus
+                        value={this.state.description}
+                        onChange={this.onDescriptionChange}
+                    />
+                    <input 
+                        type="text"
+                        placeholder="Amount"
+                        value={this.state.amount}
+                        onChange={this.onAmountChange}
+                    />
+                    <textarea 
+                        value={this.state.note}
+                        onChange={this.onNoteChange}
+                        placeholder="Add a note for your expense (Optional)">
+                    </textarea>
+                    <button>Add Expense</button>
+                </form>
+            </div>
+        )
+    };
+};
