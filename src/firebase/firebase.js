4cbdc9e4240f99f3caee9eb8258091ae0dd674cf
kc <kajalchakole@yahoo.co.in>
2021-02-19 19:41:51 +0530

    push array to firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -16,81 +16,131 @@ firebase.initializeApp(firebaseConfig);
 //working with firebase database
 const database = firebase.database();
 
-database.ref('name').once('value')
-    .then((snapshot) => {
-        const val = snapshot.val();
-        console.log(val);
-    })
-    .catch((e) => {
-        console.log('Error fetching data');
-    });
-
-
-//refer a part of the database to store data in it. if not provided any parameter, we are at the root of the database
-database.ref().set({
-    name: 'Kajal',
-    age: 30,
-    isSingle: false,
-    location: {
-        city: 'Pune',
-        country: 'INDIA'
-    }
+database.ref('expenses').push({
+    description: 'Rent',
+    note: 'This is rent',
+    amount: 109500,
+    createdAt: 9897594754 
+});
+database.ref('expenses').push({
+    description: 'Bill',
+    note: 'This is phone',
+    amount: 5900,
+    createdAt: 9894394754 
+});
+database.ref('expenses').push({
+    description: 'Food',
+    note: 'This is food bill',
+    amount: 100,
+    createdAt: 4397594754 
 });
 
 
-// database.ref().set('This is my data.');
-database.ref().update({
-    age:31
-}).then(() => {
-    console.log('Data is saved');
-}).catch((error) => {
-    console.log('Failed!', error);
-});
-
-// database.ref('age').set(30.5);
-// database.ref('location/city').set('Mumbai');
-
-//adding new attribute object
-database.ref('attributes').set({
-    height: 162,
-    weight: 56
-}).then(() => {
-    console.log('Attributes saved');
-}).catch((e) => {
-    console.log('Update failed', e);
-});
-
-database.ref('isSingle').remove()
-.then(() => {
-    console.log('isSingle removed');
-})
-.catch((e) => {
-    console.log('Could not remove data');
-});
-
-database.ref('location/city').set(null);
-
-database.ref().update({
-    age: 31,
-    name: 'Mike',
-    job: 'Developer',
-    'attributes/height': null
-});
-
-database.ref().on('value', (snapshot) => {
-    console.log(snapshot.val());
-});
-
-setTimeout(() => {
-    database.ref('age').set(26);
-}, 3500);
 
 
-setTimeout(() => {
-    database.ref().off();
-}, 7000);
+
+// database.ref('notes/MTuaNzJE7eiRzfALpvJ').remove();
 
 
-setTimeout(() => {
-    database.ref('age').set(31);
-}, 10500);
+
+// const firebaseNotes = {
+//     notes: {
+//         1: {            
+//             title: 'First Note',
+//             body: 'This is my note'
+//         },
+//         2: {            
+//             title: 'Another Note',
+//             body: 'This is my note'
+//         }
+//     }
+// };
+
+// const notes = [{
+//     id: 12,
+//     title: 'First Note',
+//     body: 'This is my note'
+// }, {    
+//     id: '75fd',
+//     title: 'Another Note',
+//     body: 'This is my note'
+// }];
+
+// database.ref('notes').set(notes);
+
+
+
+
+// database.ref('name').once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val();
+//         console.log(val);
+//     })
+//     .catch((e) => {
+//         console.log('Error fetching data');
+//     });
+
+
+// //refer a part of the database to store data in it. if not provided any parameter, we are at the root of the database
+// database.ref().set({
+//     name: 'Kajal',
+//     age: 30,
+//     isSingle: false,
+//     location: {
+//         city: 'Pune',
+//         country: 'INDIA'
+//     }
+// });
+
+
+// // database.ref().set('This is my data.');
+// database.ref().update({
+//     age:31
+// }).then(() => {
+//     console.log('Data is saved');
+// }).catch((error) => {
+//     console.log('Failed!', error);
+// });
+
+// // database.ref('age').set(30.5);
+// // database.ref('location/city').set('Mumbai');
+
+// //adding new attribute object
+// database.ref('attributes').set({
+//     height: 162,
+//     weight: 56
+// }).then(() => {
+//     console.log('Attributes saved');
+// }).catch((e) => {
+//     console.log('Update failed', e);
+// });
+
+// database.ref('isSingle').remove()
+// .then(() => {
+//     console.log('isSingle removed');
+// })
+// .catch((e) => {
+//     console.log('Could not remove data');
+// });
+
+// database.ref('location/city').set(null);
+
+// database.ref().on('value', (snapshot) => {
+//     const val = snapshot.val();
+//     console.log(`${val.name} is a ${val.job}`);
+// });
+
+// database.ref().update({
+//     age: 31,
+//     name: 'Mike',
+//     job: 'Developer',
+//     'attributes/height': null
+// });
+
+
+// setTimeout(() => {
+//         database.ref().update({
+//         name: 'Kajal',
+//         job: 'Designer',
+//     })
+// }, 3000);
