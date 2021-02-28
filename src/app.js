97273e252a12d304949f84e38d6cd50613c69ed6
kc <kajalchakole@yahoo.co.in>
2021-02-28 19:57:42 +0530

    fetch data from firebase to the application store

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-import { addExpense } from "./actions/expenses";
+import { startSetExpenses } from "./actions/expenses";
 import getVisibleExpenses from "./selectors/expenses";
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
@@ -13,22 +13,22 @@ import './firebase/firebase';
 
 const store = configureStore();
 
-store.dispatch(addExpense({
-    description:'Water Bill',
-    amount: 4500,
-    createdAt: 1000
-}));
-store.dispatch(addExpense({
-    description:'Gas Bill',
-    amount: 109500,
-    createdAt: 5000
-}));
+// store.dispatch(addExpense({
+//     description:'Water Bill',
+//     amount: 4500,
+//     createdAt: 1000
+// }));
+// store.dispatch(addExpense({
+//     description:'Gas Bill',
+//     amount: 109500,
+//     createdAt: 5000
+// }));
 
-store.dispatch(addExpense({
-    description:'Rent',
-    amount: 1000,
-    createdAt: 2000
-}));
+// store.dispatch(addExpense({
+//     description:'Rent',
+//     amount: 1000,
+//     createdAt: 2000
+// }));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
@@ -39,5 +39,13 @@ const jsx = (
         <AppRouter />
     </Provider>    
 );
+
+ReactDOM.render(<p>Loading ...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+}).catch((err) => {
+    console.log('error!');
+});
 
 ReactDOM.render(jsx, document.getElementById('app'));
