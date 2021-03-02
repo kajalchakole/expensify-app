2a2b7ac93754429016c99cce70ef7d3b49228323
kc <kajalchakole@yahoo.co.in>
2021-03-02 21:11:39 +0530

    Auth reducer

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import AppRouter, { history } from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { startSetExpenses } from "./actions/expenses";
 import getVisibleExpenses from "./selectors/expenses";
+import {login, logout} from './actions/auth';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 import 'react-dates/lib/css/_datepicker.css';
@@ -34,6 +35,7 @@ ReactDOM.render(<p>Loading ...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
     if(user) {
+        store.dispatch(login(user.uid));
         store.dispatch(startSetExpenses()).then(() => {
             renderApp();
             if(history.location.pathname === '/') {
@@ -41,6 +43,7 @@ firebase.auth().onAuthStateChanged((user) => {
             }
         });
     }else {
+        store.dispatch(logout());
         renderApp();
         history.push('/');
     }
